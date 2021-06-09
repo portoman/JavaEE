2ea0f3dea62a6cfc73cc3f855e27f13601d654c3
portobuj <portbuj@yahoo.es>
2021-06-09 17:30:01 +0200

    Update 20210609

---
+++ Leccion02/sga-jee/src/main/java/mx/com/gm/sga/servicio/PersonaServiceImpl.java
@@ -0,0 +1,41 @@
+package mx.com.gm.sga.servicio;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import mx.com.gm.sga.domain.Persona;
+
+@Stateless
+public class PersonaServiceImpl implements PersonaServiceRemote{
+
+    @Override
+    public List<Persona> listarPersonas() {
+        List<Persona> personas=new ArrayList<>();
+        personas.add(new Persona(1, "Juan", "Perez", "jperez@gmail.com","666478985"));
+        personas.add(new Persona(2, "Dulce", "Garcia", "dgarcia@gmail.com","684789456"));
+        return personas;
+    }
+
+    @Override
+    public Persona encontrarPersonaPorId(Persona persona) {
+        return null;
+    }
+
+    @Override
+    public Persona encontrarPersonaPorEmail(Persona persona) {
+        return null;
+    }
+
+    @Override
+    public void registrarPersona(Persona persona) {
+    }
+
+    @Override
+    public void modificarPersona(Persona persona) {
+    }
+
+    @Override
+    public void eliminarPersona(Persona persona) {
+    }
+    
+}
